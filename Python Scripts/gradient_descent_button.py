211b9cfba076e70946503e87a4f1907d4a68d951
jaymztheking <james.medaugh@gmail.com>
2015-06-23 16:25:16 -0600

    Clean up gradient_descent_button

--- Python Scripts/gradient_descent_button.py
+++ Python Scripts/gradient_descent_button.py
@@ -9,49 +9,52 @@ def f(a):
 def slope(a):
 	return (2*a)-10
 	
-#Used to find y-intercept in creating gradient lin	
+#Used to find y-intercept in creating gradient line	
 def intercept(a, m):
 	return m*a - f(a)
-	
-x = np.arange(0,11) #numbers 1- 10
-g = 0.1 #gamma, learning rate
-a = 8 #initial position
 
-#Initialize Graph
-fig = plt.figure()
-chrt = fig.gca()
-chrt.axis([0,10,0,30])
-fig.show()
+def main():	
+	x = np.arange(0,11) #numbers 1- 10
+	g = 0.1 #learning rate
+	a = 8 #initial position
 
-#Plot our function
-chrt.plot(x,f(x))
-fig.canvas.draw()
-i = raw_input(' ')
-while i != 'q':
-	#Plot point A
-	chrt.plot(a,f(a),'o')
-	fig.canvas.draw()
-	i = raw_input(' ')
-	
-	#Display gradient line at point A
-	m = slope(a)
-	b = intercept(a, m)
-	chrt.plot(x, (m*x)-b)
-	fig.canvas.draw()
-	i = raw_input(' ')
-	
-	#Use gradient descent algorithm to get new point
-	newA = a - (g*m)
-	print 'A is now: ', newA
-	
-	#Display new point A on Equation Line
-	chrt.plot(newA, f(newA), '^')
-	fig.canvas.draw()
-	i = raw_input(' ')
-	
-	#Set A to New A and reset
-	chrt.cla()
-	a = newA
+	#Initialize Graph
+	fig = plt.figure()
+	chrt = fig.gca()
 	chrt.axis([0,10,0,30])
+	fig.show()
+
+	#Plot our function
 	chrt.plot(x,f(x))
+	fig.canvas.draw()
+	i = raw_input(' ')
+	while i != 'q':
+		#Plot point A
+		chrt.plot(a,f(a),'o')
+		fig.canvas.draw()
+		i = raw_input(' ')
+		
+		#Display gradient line at point A
+		m = slope(a)
+		b = intercept(a, m)
+		chrt.plot(x, (m*x)-b)
+		fig.canvas.draw()
+		i = raw_input(' ')
+		
+		#Use gradient descent algorithm to get new point
+		newA = a - (g*m)
+		print 'A is now: ', newA
+		
+		#Display new point A on Equation Line
+		chrt.plot(newA, f(newA), '^')
+		fig.canvas.draw()
+		i = raw_input(' ')
+		
+		#Set A to New A and reset
+		chrt.cla()
+		a = newA
+		chrt.axis([0,10,0,30])
+		chrt.plot(x,f(x))
 	
+if __name__ == '__main__':
+	main()
