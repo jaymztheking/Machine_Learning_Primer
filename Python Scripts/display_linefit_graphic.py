493e9d91879783e3c2266f444a704912b20aa84d
jaymztheking <james.medaugh@gmail.com>
2015-06-23 11:01:43 -0600

    Add display_linefit_graphic.py
    -Add script to display regression line graphics
    -Add comments to existing scripts

---
+++ Python Scripts/display_linefit_graphic.py
@@ -0,0 +1,47 @@
+import random
+import matplotlib.pyplot as pp
+from matplotlib.lines import Line2D
+import numpy as np
+
+#Creates 10 randomish data points
+def genBoringScat():
+	x = []
+	y = []
+	for i in range(0,10):
+		x.append(random.uniform(1,10))
+		y.append(x[-1]+random.normalvariate(0,1))
+	return [x, y]
+	
+#Formats individual charts and returns error (sum of squares)
+def makeRegChart(ax, a, m, b):
+	error = 0
+	ax.axis([0,10,0,10])
+	ax.set_title('M = '+str(m)+', B = '+str(b))
+	ax.plot(a[0],a[1],'o')
+	ax.plot(a[0],np.add(np.multiply(m,a[0]),b))
+	for n in range(0,len(a[0])):
+		line = Line2D([a[0][n],a[0][n]],[a[1][n],np.add(np.multiply(m,a[0][n]),b)])
+		ax.add_line(line)
+		error += np.square(a[1][n]- np.add(np.multiply(m,a[0][n]),b))
+	return 'M = '+str(m)+' and B = '+str(b)+' has a Sum of Squares of '+str(error)
+
+#Creates 4 different regression lines to fit data	
+def showRegLines(a):
+	error = []
+	fig, axarr = pp.subplots(2, 2)
+	error.append(makeRegChart(axarr[0,0], a, 0, 5)) 
+	error.append(makeRegChart(axarr[0,1], a, .5, 2))
+	error.append(makeRegChart(axarr[1,0], a, 2, -5))
+	error.append(makeRegChart(axarr[1,1], a, 1, 0))
+	fig.canvas.draw()
+	fig.show()
+	for e in error:
+		print e
+	raw_input('')
+
+def main():
+	a = genBoringScat()
+	showRegLines(a)
+	
+if __name__ == "__main__":
+	main()
--- Python Scripts/gradient_descent_button.py
+++ Python Scripts/gradient_descent_button.py
@@ -1,43 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Function I chose to graph, (x-5)^2
 def f(a):
 	return (a-5)*(a-5)
 
+#First derivative of above function for finding slope of gradient line
 def slope(a):
 	return (2*a)-10
 	
+#Used to find y-intercept in creating gradient lin	
 def intercept(a, m):
 	return m*a - f(a)
 	
-x = np.arange(0,11)
-g = .1
-a = 7
+x = np.arange(0,11) #numbers 1- 10
+g = 0.1 #gamma, learning rate
+a = 8 #initial position
 
-
+#Initialize Graph
 fig = plt.figure()
 chrt = fig.gca()
 chrt.axis([0,10,0,30])
 fig.show()
+
+#Plot our function
 chrt.plot(x,f(x))
 fig.canvas.draw()
 i = raw_input(' ')
 while i != 'q':
-	#Plot point
+	#Plot point A
 	chrt.plot(a,f(a),'o')
 	fig.canvas.draw()
 	i = raw_input(' ')
 	
-	#Display gradient line
+	#Display gradient line at point A
 	m = slope(a)
 	b = intercept(a, m)
 	chrt.plot(x, (m*x)-b)
 	fig.canvas.draw()
 	i = raw_input(' ')
 	
+	#Use gradient descent algorithm to get new point
 	newA = a - (g*m)
 	print 'A is now: ', newA
-	#Display new A on Equation Line
+	
+	#Display new point A on Equation Line
 	chrt.plot(newA, f(newA), '^')
 	fig.canvas.draw()
 	i = raw_input(' ')
